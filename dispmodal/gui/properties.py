7f0998ef7b4b35f74f3a391231d8e7ce47c7be59
Demian Volkov <demianwolfssd@gmail.com>
2022-06-11 22:03:45 +0300

    Add style for Property.TEntry

--- dispmodal/gui/properties.py
+++ dispmodal/gui/properties.py
@@ -1,9 +1,13 @@
 import tkinter as tk
 import tkinter.ttk as ttk
-
+import tkinter.font as tk_font
 
 class PropertyEntry(ttk.Entry):
     def __init__(self, *args, **kwargs):
+        kwargs.setdefault("style", "Property.TEntry")
+        kwargs.setdefault("justify", "center")
+        kwargs.setdefault("font", tk_font.Font(size=12))
+
         super().__init__(*args, **kwargs)
 
         ttk.Button(self, text="\u2398").pack(side=tk.RIGHT)
@@ -13,6 +17,6 @@ class PropertiesFrame(ttk.Frame):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        PropertyEntry(self).pack(fill=tk.X, pady=5)
-        PropertyEntry(self).pack(fill=tk.X, pady=5)
-        PropertyEntry(self).pack(fill=tk.X, pady=5)
+        PropertyEntry(self).pack(fill=tk.X, pady=5, ipady=10)
+        PropertyEntry(self).pack(fill=tk.X, pady=5, ipady=10)
+        PropertyEntry(self).pack(fill=tk.X, pady=5, ipady=10)
--- dispmodal/gui/style.py
+++ dispmodal/gui/style.py
@@ -5,6 +5,11 @@ STYLE = {
     "widgets": {
         "TFrame": {
             "background": "white",
-        }
+        },
+
+        "Property.TEntry": {
+            "fieldbackground": "#F8F9FA",
+            "borderwidth": 0,
+        },
     }
 }
